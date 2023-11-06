4e99aca0b71278dc002f4bd07360fd97c37513f0
sidee <said.issack@student.moringaschool.com>
2023-11-06 11:06:34 +0300

    created the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as Components from './components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    const [signIn, toggle] = React.useState(true);
+     return(
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                     <Components.Title>Create Account</Components.Title>
+                     <Components.Input type='text' placeholder='Name' />
+                     <Components.Input type='email' placeholder='Email' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Components.Button>Sign Up</Components.Button>
+                 </Components.Form>
+             </Components.SignUpContainer>
